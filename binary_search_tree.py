d7fde94cc6a6f64275a221c4e5c8a10018b28523
Ashish Kumar <56558070+ashishkumar1409@users.noreply.github.com>
2019-10-21 16:37:25 +0530

    Create binary_search_tree.py
    Binary Search Tree implementation in Python

---
+++ binary_search_tree.py
@@ -0,0 +1,62 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+        
+    def preorder_search(self,start,find_val):
+        if start:
+            if start == find_val:
+                return True
+            self.preorder_search(start.left,find_val)
+            self.preorder_search(start.right,find_val)
+        return False
+    
+    def preorder_print(self,start,traversal):
+        if start:
+            traversal.append(start.value)
+            self.preorder_print(start.left,traversal)
+            self.preorder_print(start.right,traversal)
+        return traversal
+    
+        
+    def search(self, find_val):
+        """Return True if the value
+        is in the tree, return
+        False otherwise."""
+        return self.preorder_search(tree.root,find_val)
+
+    
+    def print_tree(self):
+        """Print out all tree nodes
+        as they are visited in
+        a pre-order traversal."""
+        return self.preorder_print(tree.root,[])
+
+
+        
+
+
+        
+
+
+# Set up tree
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+
+# Test search
+# Should be True
+print tree.search(4)
+# Should be False#
+print tree.search(6)
+
+# Test print_tree
+# Should be 1-2-4-5-3
+print tree.print_tree()
